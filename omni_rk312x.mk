7ee1b0313942e92d982565ac29e57e3e997d2ddd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-04 22:14:19 +0200

    rk312x: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: rockchip Model: U704RC Script version: 2.4.0

---
+++ omni_rk312x.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from rk312x device
+$(call inherit-product, device/rockchip/rk312x/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := rk312x
+PRODUCT_NAME := omni_rk312x
+PRODUCT_BRAND := rockchip
+PRODUCT_MODEL := U704RC
+PRODUCT_MANUFACTURER := rockchip
